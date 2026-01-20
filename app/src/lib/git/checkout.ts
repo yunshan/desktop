e2587bee4a31baa8bae42acf1df5b7b9f91dfecb
Markus Olsson <j.markus.olsson@gmail.com>
2026-01-20 15:00:28 +0100

    Whoops

--- app/src/lib/git/checkout.ts
+++ app/src/lib/git/checkout.ts
@@ -7,6 +7,7 @@ import {
   executionOptionsWithProgress,
 } from '../progress'
 import { AuthenticationErrors } from './authentication'
+import { enableRecurseSubmodulesFlag } from '../feature-flag'
 import {
   envForRemoteOperation,
   getFallbackUrlForProxyResolve,
